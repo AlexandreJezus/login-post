124c091aa2b9baf4cfb72cdc36f7d88f16ab5e30
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-10-08 08:46:45 -0300

    fix authenticator

--- src/middlewares/jwt-authenticator.js
+++ src/middlewares/jwt-authenticator.js
@@ -3,18 +3,18 @@ import User from "../models/user-model.js";
 
 const jwtauthenticator = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")(1);
+    const token = req.headers.authorization.split(" ")[1];
     const user = jwtService.verifyAcessToken(token);
 
     if (user) {
       req.user = User.findById(user._id).exec();
+      console.log(req.user);
       next();
     } else {
-      throw new Error("");
+      throw new Error();
     }
-    throw new Error();
   } catch (error) {
-    res.sendStatus(401).send(error.message);
+    res.sendStatus(401);
   }
 };
 export default jwtauthenticator;
